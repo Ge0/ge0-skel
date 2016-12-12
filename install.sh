83a8f80cf0501a8bc534f1a6d1e5433bcc2ea36b
Geoffrey ROYER <geoffrey.royer@gmail.com>
2016-12-12 13:10:23 +0100

    Fix .config creation directory

--- install.sh
+++ install.sh
@@ -7,5 +7,6 @@ $PREFIX ./dotfiles/.tmux_handler $HOME/.tmux_handler
 mkdir -p ~/envs
 $PREFIX ./dotfiles/.virtualenvwrapper $HOME/.virtualenvwrapper
 $PREFIX ./dotfiles/.zshrc $HOME/.zshrc
-mkdir -p /.config
+
+mkdir -p ~/.config
 $PREFIX ./nvim $HOME/nvim
