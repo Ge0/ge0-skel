9c3310e3a9d96c9e88727e6aa15b5a7d413944f6
Geoffrey ROYER <geoffrey.royer@gmail.com>
2016-12-12 13:10:12 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 PREFIX='ln -srf'
+$PREFIX ./dotfiles/.Xresources $HOME/.Xresources
+$PREFIX ./dotfiles/.aliases $HOME/.aliases
+$PREFIX ./dotfiles/.tmux.conf $HOME/.tmux.conf
+$PREFIX ./dotfiles/.tmux_handler $HOME/.tmux_handler
+
+mkdir -p ~/envs
+$PREFIX ./dotfiles/.virtualenvwrapper $HOME/.virtualenvwrapper
 $PREFIX ./dotfiles/.zshrc $HOME/.zshrc
 mkdir -p /.config
 $PREFIX ./nvim $HOME/nvim
