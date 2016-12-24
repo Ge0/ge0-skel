637d8d17bb84fd4a7168d2d5385b2876f3d66ddd
Geoffrey ROYER <geoffrey.royer@gmail.com>
2016-12-24 17:37:52 +0100

    Add vim-plug install script

---
+++ install_vim-plug.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+VIMPLUG=~/.local/share/nvim/site/autoload/plug.vim
+[[ -e $VIMPLUG ]] && \
+	echo "vim-plug already set up. Skipping..." && exit 1
+curl -fLo $VIMPLUG --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
