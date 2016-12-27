0ae6332080be2472599b1e79abc8bfd3cddfd516
Ge0 <geoffrey.royer@gmail.com>
2016-12-27 20:02:08 +0100

    Add script to install files on server

---
+++ install_server.sh
@@ -0,0 +1,13 @@
+PREFIX='ln -sf'
+CURDIR=`pwd`
+$PREFIX $CURDIR/dotfiles/.aliases $HOME/.aliases
+$PREFIX $CURDIR/dotfiles/.path $HOME/.path
+$PREFIX $CURDIR/dotfiles/.tmux.conf $HOME/.tmux.conf
+$PREFIX $CURDIR/dotfiles/.tmux_handler $HOME/.tmux_handler
+
+mkdir -p ~/envs
+$PREFIX $CURDIR/dotfiles/.virtualenvwrapper $HOME/.virtualenvwrapper
+$PREFIX $CURDIR/dotfiles/.zshrc $HOME/.zshrc
+
+mkdir -p ~/.config
+$PREFIX $CURDIR/nvim $HOME/.config/nvim
